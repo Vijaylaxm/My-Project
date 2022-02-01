c918ef3d7ac32f538b57462cfacffa38653d02aa
Vijaylaxm <mathlaxmi12@gmail.com>
2022-02-01 11:44:26 +0530

    first commit

---
+++ src/main/java/com/example/vijaylaxmi/BankEchoSystemApplication.java
@@ -0,0 +1,14 @@
+package com.example.vijaylaxmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankEchoSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankEchoSystemApplication.class, args);
+		System.out.println("Spring");
+	}
+
+}
---
+++ src/main/java/com/example/vijaylaxmi/Echodemo.java
@@ -0,0 +1,19 @@
+package com.example.vijaylaxmi;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Echodemo {
+	
+	
+
+	@GetMapping(path="/hii")
+	@ResponseBody
+	public String getCustomerDetails() {
+	return "This is test with echo";
+
+
+}
+}
